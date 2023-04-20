f9589e2994422cd242df9bf386dc55f4c9565243
LisaGrüner <elisabeth.gruener@freenet.de>
2023-04-20 18:04:22 +0200

    layers add to map

--- main.js
+++ main.js
@@ -14,10 +14,10 @@ let map = L.map("map").setView([
 
 //thematische Layer
 let themaLayer = {
-    stops: L.featureGroup(),
-    lines: L.featureGroup(),
-    zones: L.featureGroup(),
-    sites: L.featureGroup()
+    stops: L.featureGroup().addTo(map),
+    lines: L.featureGroup().addTo(map),
+    zones: L.featureGroup().addTo(map),
+    sites: L.featureGroup().addTo(map)
 }
 
 // Hintergrundlayer, leaftlet Provider
